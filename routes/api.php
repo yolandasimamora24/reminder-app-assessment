5e9f06c855b6ffefb27045193c77fc7014ff25e6
Yolanda Simamora <nanda_yuas@yahoo.com>
2024-09-30 14:00:44 +0700

    feat:deploy to vercel

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\v1\AuthController;
-use App\Http\Controllers\Api\v1\InsuranceController;
-use App\Http\Controllers\Api\v1\AddressController;
-use App\Http\Controllers\Api\v1\ProviderController;
-use App\Http\Controllers\Api\v1\ReviewController;
-use App\Http\Controllers\Api\v1\SymptomController;
-use App\Http\Controllers\Api\v1\SpecializationSymptomController;
-use App\Http\Controllers\Api\v1\SpecializationController;
-use App\Http\Controllers\Api\v1\UserController;
-use App\Http\Controllers\Api\v1\JobApplicationController;
-use App\Http\Controllers\Api\v1\PrescriptionController;
-use App\Http\Controllers\Api\v1\DiagnosticController;
-use App\Http\Controllers\Api\v1\MedicineController;
-use App\Http\Controllers\Api\v1\ConditionController;
-use App\Http\Controllers\Api\v1\AllergyController;
-use App\Http\Controllers\Api\v1\AppointmentController;
-use App\Http\Controllers\Api\v1\ConsultationController;
-use App\Http\Controllers\Api\v1\FilterController;
-use App\Http\Controllers\Api\v1\LabController;
-use App\Http\Controllers\Api\v1\BillingController;
-use App\Http\Controllers\Api\v1\AppointmentWebhookController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
